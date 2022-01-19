796fae2b00d4d84cbda0d505c5d026384fd08d98
fom78 <fom78a@gmail.com>
2022-01-18 21:34:10 -0300

    initial commit and first components

---
+++ src/components/List.js
@@ -0,0 +1,16 @@
+const List = () => {
+    return (
+        <>
+            <h1>List of operations</h1>
+            <div>
+                <div>Concept</div>
+                <div>Amount</div>
+                <div>Date</div>
+                <div>Type</div>
+                <div>Action</div>
+            </div>
+        </>
+    )
+}
+
+export default List;
