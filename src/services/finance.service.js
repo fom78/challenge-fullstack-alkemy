094e1a76e5987a39b762946261666c5fe0b4a44c
fom78 <fom78a@gmail.com>
2022-01-22 09:23:38 -0300

    add axios and first service

---
+++ src/services/finance.service.js
@@ -0,0 +1,28 @@
+import axios from 'axios'
+
+const API_URL = `http://localhost:4000/operations`
+
+class FinanceService {
+  create (data) {
+    return axios.post(API_URL, data)
+  }
+
+  update (id, data) {
+    return axios.put(API_URL + id, data)
+  }
+
+  delete (id) {
+    return axios.delete(API_URL + id)
+  }
+
+  get (id) {
+    return axios.get(API_URL + id)
+  }
+
+  getAll () {
+    return axios.get(API_URL)
+  }
+
+}
+
+export default new FinanceService()
