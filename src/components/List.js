b58a5e6854e5a8f2daa4925f1eff25805717e182
Fernando <fom78a@gmail.com>
2022-01-24 11:23:47 -0300

    refactor, add Operation component

--- src/components/List.js
+++ src/components/List.js
@@ -1,72 +1,29 @@
-import { Outlet, useNavigate } from 'react-router-dom'
+import { Outlet } from 'react-router-dom'
 // Components
-import Edit from './icons/Edit'
-import Delete from './icons/Delete'
+import Operation from './Operation'
 // Notify
-import { toast } from 'react-toastify'
+// import { toast } from 'react-toastify'
 // Services
-import FinanceService from 'services/finance.service'
+// import FinanceService from 'services/finance.service'
 // Styles
 import styled from 'styled-components'
 
 const List = ({ operations, setRefreshList, title = 'List of operations', actions = true }) => {
-  const navigate = useNavigate()
-
-  const handleEdit = (id) => {
-    navigate(`/list/edit/${id}`)
-  }
-  const handleDelete = (id) => {
-    navigate(`/list/delete/${id}`)
-  }
-  const handleDelete1 = (id) => {
-    FinanceService.delete(id)
-      .then((response) => {
-        setRefreshList(true)
-        toast.success(`Operation id: ${id} deleted`, {
-          position: 'top-center',
-          autoClose: 5000,
-          hideProgressBar: false,
-          closeOnClick: true,
-          pauseOnHover: true,
-          draggable: true,
-          progress: undefined
-        })
-      })
-      .catch((e) => {
-        console.log(e)
-      })
-  }
-
   return (
     <>
       <Outlet />
       <h2>{title}</h2>
       <ListStyled>
         {operations && operations.map(operation => {
-          // Convert date to short date, format: d/mm/yy
-          const date = new Date(operation.date)
-          const formatedDate = new Intl.DateTimeFormat('es-AR', { year: '2-digit', month: '2-digit', day: 'numeric' }).format(date)
-          const color = (operation.type === 'income') ? 'green' : 'red'
           return (
-            <Operation key={operation.id}>
-              <div className='operationHeader'>
-                <div className='date'>{formatedDate}</div>
-                <div className={`type ${color}`}>{operation.type}</div>
-                {actions
-                  ? (
-                    <div className='btn-action'>
-                      <button onClick={() => handleEdit(operation.id)}><Edit /></button>
-                      <button><Delete onClick={() => handleDelete1(operation.id)} /></button>
-                      <button><Delete onClick={() => handleDelete(operation.id)} /></button>
-                    </div>)
-                  : null}
-              </div>
-              <p className='operationConcept'>{operation.concept}</p>
-              <div className='operationAmount'>Amount: <span>$ {operation.amount}</span></div>
-            </Operation>
+            <Operation
+              key={operation.id}
+              operation={operation}
+              actions={actions}
+              setRefreshList={setRefreshList}
+            />
           )
         })}
-
       </ListStyled>
     </>
   )
@@ -77,57 +34,4 @@ export default List
 export const ListStyled = styled.div`
   padding-bottom: 1.5rem;
   background-color: var(--white);
-`
-
-const Operation = styled.div`
-  display: flex;
-  flex-direction: column;
-  height: 120px;
-  max-width: 100%;
-  margin-bottom: 1rem;
-  padding: 0.3rem;
-  background-color: var(--bg-primary);
-  color: var(--text-primary);
-  font-size: .8rem;
-  
-  & .operationHeader {
-    width: 100%;
-    font-size: .7rem;
-    padding-bottom: .5rem;
-    display: flex;
-    justify-content: space-between;
-
-    & .type {
-      text-transform: uppercase;
-      margin:auto;
-      padding: 4px;
-    }
-    & .green {border: 2px solid var(--green);}
-    & .red {border: 2px solid var(--red);}
-
-  }
-
-  & .operationConcept {
-    color: var(--text-primary);
-    padding-left: 1rem;
-  }
-
-  & .operationAmount {
-    margin-bottom: .5rem ;
-    & > span {
-    font-weight: bold;
-  }
-  }
-
-  & .btn-action > button {
-    margin-right: .5rem;
-    background-color: transparent;
-    border: 1px solid transparent;
-    cursor: pointer;
-    color: var(--white);
-    
-    &:hover {
-      border: 1px solid var(--white);
-    }    
-  } 
 `
---
+++ src/components/Operation.js
@@ -0,0 +1,135 @@
+import { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
+// Components
+import ConfirmDeleteModal from './ConfirmDeleteModal'
+import Edit from './icons/Edit'
+import Delete from './icons/Delete'
+import Spinner from './Spinner'
+// Notify
+import { toast } from 'react-toastify'
+// Services
+import FinanceService from 'services/finance.service'
+// Styles
+import styled from 'styled-components'
+
+const Operation = ({ operation, actions, setRefreshList }) => {
+  const [stateModal, setStateModal] = useState(false)
+  const [isDeleting, setIsDeleting] = useState(false)
+  const navigate = useNavigate()
+
+  // Convert date to short date, format: d/mm/yy
+  const date = new Date(operation.date)
+  const formatedDate = new Intl.DateTimeFormat('es-AR', { year: '2-digit', month: '2-digit', day: 'numeric' }).format(date)
+  const color = (operation.type === 'income') ? 'green' : 'red'
+
+  const handleEdit = (id) => {
+    navigate(`/list/edit/${id}`)
+  }
+
+  const handleDelete = (id) => {
+    setIsDeleting(true)
+    FinanceService.delete(id)
+      .then((response) => {
+        setRefreshList(true)
+        toast.success(`Operation id: ${id} deleted`, {
+          position: 'top-center',
+          autoClose: 1000,
+          hideProgressBar: false,
+          closeOnClick: true,
+          pauseOnHover: true,
+          draggable: true,
+          progress: undefined
+        })
+        setIsDeleting(false)
+      })
+      .catch((e) => {
+        console.log(e)
+      })
+  }
+
+  if (isDeleting) {
+    return <Spinner />
+  }
+
+  return (
+    <OperationStyled key={operation.id}>
+      <div className='operationHeader'>
+        <div className='date'>{formatedDate}</div>
+        <div className={`type ${color}`}>{operation.type}</div>
+        {actions
+          ? (
+            <div className='btn-action'>
+              <button onClick={() => handleEdit(operation.id)}><Edit /></button>
+              <button><Delete onClick={() => setStateModal(!stateModal)} /></button>
+            </div>)
+          : null}
+      </div>
+      <p className='operationConcept'>{operation.concept}</p>
+      <div className='operationAmount'>Amount: <span>$ {operation.amount}</span></div>
+
+      <ConfirmDeleteModal
+        state={stateModal}
+        changeState={setStateModal}
+        handleDelete={() => handleDelete(operation.id)}
+        handleEdit={() => handleEdit(operation.id)}
+        operation={operation}
+        showOverlay
+      />
+    </OperationStyled>
+  )
+}
+
+export default Operation
+
+const OperationStyled = styled.div`
+  display: flex;
+  flex-direction: column;
+  height: 120px;
+  max-width: 100%;
+  margin-bottom: 1rem;
+  padding: 0.3rem;
+  background-color: var(--bg-primary);
+  color: var(--text-primary);
+  font-size: .8rem;
+  
+  & .operationHeader {
+    width: 100%;
+    font-size: .7rem;
+    padding-bottom: .5rem;
+    display: flex;
+    justify-content: space-between;
+
+    & .type {
+      text-transform: uppercase;
+      margin:auto;
+      padding: 4px;
+    }
+    & .green {border: 2px solid var(--green);}
+    & .red {border: 2px solid var(--red);}
+
+  }
+
+  & .operationConcept {
+    color: var(--text-primary);
+    padding-left: 1rem;
+  }
+
+  & .operationAmount {
+    margin-bottom: .5rem ;
+    & > span {
+    font-weight: bold;
+  }
+  }
+
+  & .btn-action > button {
+    margin-right: .5rem;
+    background-color: transparent;
+    border: 1px solid transparent;
+    cursor: pointer;
+    color: var(--white);
+    
+    &:hover {
+      border: 1px solid var(--white);
+    }    
+  } 
+`
