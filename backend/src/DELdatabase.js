0fbb5709f4a9dde925436e2ab5c2a837f16218c4
fom78 <fom78a@gmail.com>
2022-02-12 19:44:23 -0300

    back:fix cors

--- backend/src/DELdatabase.js
+++
@@ -1,9 +0,0 @@
-import mysql from 'mysql'
-// Config
-import config from './config'
-
-export const con = mysql.createConnection({
-  user: config.AWS_USER,
-  host: config.AWS_HOST,
-  password: config.AWS_PASSWORD
-})
