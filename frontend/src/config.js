44d9f0e181d35e815ec813e6dd37da1cf4a5b0e5
fom78 <fom78a@gmail.com>
2022-02-13 16:27:45 -0300

    front: access to api host by env variable

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,3 +1,2 @@
-// export const HOST = 'http://localhost:4000/api/v1/'
 
-export const HOST = 'https://personal-finance-alkemy.herokuapp.com/api/v1/'
+export const HOST = process.env.REACT_APP_PUBLIC_HOST || 'http://localhost:4000/api/v1/'
--- frontend/src/services/categories.service.js
+++ frontend/src/services/categories.service.js
@@ -4,22 +4,6 @@ import { HOST } from 'config'
 const API_URL = `${HOST}categories/`
 
 class CategoriesService {
-  create (data) {
-    return axios.post(API_URL, data)
-  }
-
-  update (id, data) {
-    return axios.put(API_URL + id, data)
-  }
-
-  delete (id) {
-    return axios.delete(API_URL + id)
-  }
-
-  get (id) {
-    return axios.get(API_URL + id)
-  }
-
   getAll () {
     return axios.get(API_URL)
   }
